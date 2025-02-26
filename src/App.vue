04211ab361ab89ed3cd70b112fea8c4b8189e6c9
Alejandro Agustin Collado <alejandro.agustincollado@gmail.com>
2025-02-25 21:06:33 -0300

    Final NT2

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="jumbotron">
+      <h1 class="display-4">CALCULADORA DE CAMBIO</h1>
+      <p class="lead">Convierte dólares a la moneda local fácilmente.</p>
+    </div>
+    
+    
+    <ConvertidorUSD /> 
+    <hr>
+    <p><b>Multiple choice:</b></p>
+    <ul>
+      <li>1: C</li>
+      <li>2: B</li>
+      <li>3: C</li>
+      <li>4: A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import ConvertidorUSD from './componentes/ConvertidorUSD.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      ConvertidorUSD
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Arial;
+    margin: 20px;
+  }
+</style>
---
+++ src/componentes/ConvertidorUSD.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="src-componentes-pantalla-dolar">
+    <h2>Conversor a USD</h2>
+    
+    <label>
+      Monto en ARS: <input type="number" v-model="pesos">
+    </label>
+
+    <label>
+      Cotización USD: <input type="number" v-model="cotizacion" :readonly="actualizacion">
+    </label>
+
+    <label>
+      <input type="checkbox" v-model="autoActualizarCotizacion"> actualizar
+    </label>
+
+    <p v-if="ultimaActualizacion">
+      Última actualización: <strong>{{ ultimaActualizacion }}</strong>
+    </p>
+
+    <p>
+      Total en USD: <strong>{{ convertidorPesosADolar }}</strong>
+    </p>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'src-componentes-pantalla-dolar',
+  data () {
+    return {
+      pesos: '',
+      cotizacion: '',
+      valorConvertido: '', 
+      autoActualizarCotizacion: false, 
+      temporizador: null, 
+      ultimaActualizacion: '' 
+    }
+  },
+  watch: {
+    autoActualizarCotizacion(valorBool) {
+      if (valorBool) {
+        this.comenzarActualizacion();
+      } else {
+        this.detenerActualizacion();
+      }
+    }
+  },
+  methods: {
+    async obtenerCotizacion() {
+      const respuesta = await axios.get('https://api.bluelytics.com.ar/v2/latest')
+      this.cotizacion = respuesta.data.blue.value_sell;
+      this.ultimaActualizacion = new Date().toLocaleString();
+    },
+
+    comenzarActualizacion() {
+      this.obtenerCotizacion();
+      this.temporizador = setInterval(() => {
+        this.obtenerCotizacion();
+        this.ultimaActualizacion = new Date().toLocaleString();
+      }, 2000);
+    },
+
+  detenerActualizacion() {
+  clearInterval(this.temporizador);  
+}
+
+  },
+  computed: {
+    convertidorPesosADolar() {
+      if (this.pesos && this.cotizacion) {
+        return (this.pesos / this.cotizacion).toFixed(2);
+      }
+      return '';
+    }
+  }
+}
+</script>
+
+<style scoped lang="css">
+  
+</style>
